a59cf3b5da96474c5fcce30b3821ba734cfc1f9f
rzr- <axel.hilmi@mail.ru>
2019-05-05 21:13:44 +0300

    procfile added

--- configuration.py
+++ configuration.py
@@ -1,30 +1,32 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
     FLASK_CONFIG = 'production'
-    SECRET_KEY = 'SECRET_KEY'
-    SECURITY_PASSWORD_SALT = 'LONG_BIG_PASSWORD_SALT_VERY_SALTED'
-    WTF_CSRF_SECRET_KEY = 'SOME_SECRET_KEY'
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SECURITY_PASSWORD_SALT = os.environ['LONG_BIG_PASSWORD_SALT_VERY_SALTED']
+    WTF_CSRF_SECRET_KEY = os.environ['SOME_SECRET_KEY']
 
-    SQLALCHEMY_DATABASE_URI = 'mysql://address_to_the_databease'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_POOL_TIMEOUT = 20
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = '465'
+    MAIL_SERVER = os.environ['MAIL_SERVER']
+    MAIL_PORT = os.environ['MAIL_PORT']
     #MAIL_USE_TLS = 'False'
     MAIL_USE_SSL = 'True'
-    MAIL_USERNAME = 'email'
-    MAIL_PASSWORD = 'email_password'
-    MAIL_DEFAULT_SENDER = 'default_sender'
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    MAIL_DEFAULT_SENDER = os.environ['MAIL_DEFAULT_SENDER']
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_ECHO = True
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql://address_to_the_databease'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class TestingConfig(Config):
     TESTING = True

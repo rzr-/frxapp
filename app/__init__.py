d14e4f6294b56a28fd68e9426c84d1ca4e8bd73d
rzr- <axel.hilmi@mail.ru>
2019-05-06 12:29:06 +0300

    import fix

--- app/__init__.py
+++ app/__init__.py
@@ -10,53 +10,44 @@ from . import configuration
 # after the db variable initialization
 login_manager = LoginManager()
 
-def create_app(config):
-    app = Flask(__name__)
-    app.config.from_object(config)
+app = Flask(__name__)
+app.config.from_object(configuration.DevelopmentConfig)
 
-    Bootstrap(app)
-    # db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_message = "You must be logged in to access this page."
-    login_manager.login_view = "auth.login"
-
-    from app import models
-
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
-
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
-    from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint)
-
-    from .user import user as user_blueprint
-    app.register_blueprint(user_blueprint)
-
-    @app.errorhandler(403)
-    def forbidden(error):
-        return render_template('errors/403.html', title='Forbidden'), 403
-
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return render_template('errors/404.html', title='Page Not Found'), 404
-
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return render_template('errors/500.html', title='Server Error'), 500
-
-    @app.route('/500')
-    def error():
-        abort(500)
-
-    return app
-
-app = create_app(configuration.DevelopmentConfig)
-
+Bootstrap(app)
+# db.init_app(app)
 # db variable initialization
 db = SQLAlchemy(app)
-
-
-
+login_manager.init_app(app)
 migrate = Migrate(app, db)
+login_manager.login_message = "You must be logged in to access this page."
+login_manager.login_view = "auth.login"
+
+from app import models
+
+from .admin import admin as admin_blueprint
+app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+from .home import home as home_blueprint
+app.register_blueprint(home_blueprint)
+
+from .user import user as user_blueprint
+app.register_blueprint(user_blueprint)
+
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('errors/403.html', title='Forbidden'), 403
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html', title='Page Not Found'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('errors/500.html', title='Server Error'), 500
+
+@app.route('/500')
+def error():
+    abort(500)
